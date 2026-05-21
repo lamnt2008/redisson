437e3d92ccee17cf4aad77fe4d689c26cac631eb
lamnt2008 <lamnt.hust2000@gmail.com>
2026-05-21 11:29:34 +0700

    #7142 add-RArray-object
    Signed-off-by: lamnt2008 <lamnt.hust2000@gmail.com>

---
+++ redisson/src/test/java/org/redisson/client/protocol/decoder/ArrayInfoDecoderTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2013-2026 Nikita Koksharov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.redisson.client.protocol.decoder;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.array.ArrayInfo;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArrayInfoDecoderTest {
+
+    @Test
+    public void testDecodeSkipsMissingFields() {
+        ArrayInfo info = new ArrayInfoDecoder().decode(Arrays.asList("count", 2L, "len", 5L), null);
+
+        assertThat(info.getCount()).isEqualTo(2);
+        assertThat(info.getLength()).isEqualTo(5);
+        assertThat(info.getNextInsertIndex()).isZero();
+        assertThat(info.getDenseSlices()).isNull();
+    }
+
+    @Test
+    public void testDecodeSkipsNullAndOddTailFields() {
+        ArrayInfo info = new ArrayInfoDecoder().decode(Arrays.asList("count", 2L, "slice-size", null, "dangling"), null);
+
+        assertThat(info.getCount()).isEqualTo(2);
+        assertThat(info.getSliceSize()).isZero();
+    }
+
+    @Test
+    public void testDecodeStringValues() {
+        ArrayInfo info = new ArrayInfoDecoder().decode(Arrays.asList(
+                "count", "2",
+                "len", "5",
+                "avg-dense-size", "1.5"), null);
+
+        assertThat(info.getCount()).isEqualTo(2);
+        assertThat(info.getLength()).isEqualTo(5);
+        assertThat(info.getAverageDenseSize()).isEqualTo(1.5);
+    }
+
+}
